848f09ba4c89887cbbdc305728a86f2f0a372b09
Radha Singh <radhasingh2859@gmail.com>
2026-08-18 10:58:54 +0530

    display multiples of 5 and 3

---
+++ pratice1.java
@@ -0,0 +1,13 @@
+
+public class pratice1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 200; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
