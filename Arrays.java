ecaddd77acb4193c77a4e22707470caadcbde654
Radha Singh <radhasingh2859@gmail.com>
2026-08-19 21:16:16 +0530

    reversed array

--- Arrays.java
+++ Arrays.java
@@ -1,22 +1,30 @@
 
+import java.util.*;
+
 public class Arrays {
 
     public static void main(String[] args) {
-        int arr[] = {10, 25, 3, 48, 17};
-        int smallest = arr[0];
-        int largest = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < smallest) {
-                smallest = arr[i];
-
-            }
-            if (arr[i] > largest) {
-                largest = arr[i];
-            }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
 
         }
-        System.out.println("smallest" + smallest);
-        System.out.println("largest" + largest);
-    }
+        System.out.println("Reversed array");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
+    }
 }
