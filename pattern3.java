570420f261ec50c050f13ef57c4003b2ecb945a8
Radha Singh <radhasingh2859@gmail.com>
2026-08-19 09:25:26 +0530

    number pyramid

--- pattern3.java
+++ pattern3.java
@@ -3,11 +3,12 @@ public class pattern3 {
 
     public static void main(String[] args) {
         int n = 4;
-        int a = 1;
         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= i; j++) {
-                System.out.print(a + " ");
-                a++;
+                System.out.print(i + " ");
 
             }
             System.out.println();
