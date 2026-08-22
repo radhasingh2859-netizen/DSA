713726ab2a9c24462394b78e6cb6efc21d432610
Radha Singh <radhasingh2859@gmail.com>
2026-08-22 16:52:54 +0530

    merged array

--- Arrays.java
+++ Arrays.java
@@ -5,32 +5,37 @@ public class Arrays {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size element");
+        System.out.println("Enter the size of first array");
         int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.println("Enter the element");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int uni[] = new int[n];
-        int count = 0;
+        System.out.println("enter the size of second array");
+
+        int n1 = sc.nextInt();
+        int arr1[] = new int[n];
+        System.out.println("Enter the element in first array");
+
         for (int i = 0; i < n; i++) {
-            boolean dup = false;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == uni[j]) {
-                    dup = true;
-                }
+            arr1[i] = sc.nextInt();
 
-            }
-            if (!dup) {
-                uni[count] = arr[i];
-                count++;
-
-            }
         }
-        System.out.println("array without duplicate number");
-        for (int i = 0; i <= count; i++) {
-            System.out.print(uni[i] + " ");
+        System.out.println("Enter the element in second array");
+        int arr2[] = new int[n1];
+        for (int i = 0; i < n; i++) {
+            arr2[i] = sc.nextInt();
+
+        }
+        int arr3[] = new int[n + n1];
+        for (int i = 0; i < n; i++) {
+
+            arr3[i] = arr1[i];
+        }
+        for (int i = 0; i < n1; i++) {
+            arr3[i + n1] = arr2[i];
+
+        }
+        System.out.println("merge array");
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i] + " ");
+
         }
 
     }
