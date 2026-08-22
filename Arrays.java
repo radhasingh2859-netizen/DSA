7c9e39da343c804430249c1c3b479e04571a3ebc
Radha Singh <radhasingh2859@gmail.com>
2026-08-22 10:41:02 +0530

    duplicate

--- Arrays.java
+++ Arrays.java
@@ -5,28 +5,33 @@ public class Arrays {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of elements: ");
+        System.out.println("Enter the size element");
         int n = sc.nextInt();
         int arr[] = new int[n];
-
-        System.out.println("Enter the elements");
+        System.out.println("Enter the element");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
-
         }
-        int largest = arr[0];
-        int secondlargest = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > largest) {
-                secondlargest = largest;
-                largest = arr[i];
+        int uni[] = new int[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            boolean dup = false;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == uni[j]) {
+                    dup = true;
+                }
 
-            } else if (arr[i] > secondlargest && arr[i] != largest) {
-                secondlargest = arr[i];
             }
+            if (!dup) {
+                uni[count] = arr[i];
+                count++;
 
+            }
         }
-        System.out.println("second largest element" + secondlargest);
+        System.out.println("array without duplicate number");
+        for (int i = 0; i <= count; i++) {
+            System.out.print(uni[i] + " ");
+        }
+
     }
 }
