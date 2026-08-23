c64857bf09db34fb7f085004fd3832cb6b9d38ac
Radha Singh <radhasingh2859@gmail.com>
2026-08-23 21:44:13 +0530

    left rotation by d

--- Arrays.java
+++ Arrays.java
@@ -5,26 +5,23 @@ public class Arrays {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of elenent");
+        System.out.println("Enter the size of element");
         int n = sc.nextInt();
+        System.out.println("enter the value of d to move to left ");
+        int d = sc.nextInt();
         int arr[] = new int[n];
-        //input elements
+        int arr1[] = new int[n + d];
+
+        System.out.print("enter the element");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i = 0; i < n; i++) {
-            int count = 1;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
-                    arr[j] = -1;
-                }
-            }
-            if (arr[i] != -1) {
-                System.out.println(arr[i] + " occurs " + count + " times");
+        for (int i = 0; i < arr.length + d; i++) {
+            arr1[i] = arr[(i + d) % n];
 
-            }
+        }
+        for (int i = 0; i < n + d; i++) {
+            System.out.print(arr1[i]);
         }
     }
-
 }
