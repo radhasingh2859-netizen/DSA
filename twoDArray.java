a2a18e79e7c1e2750981c52a5f22287a5fd2c581
Radha Singh <radhasingh2859@gmail.com>
2026-08-24 18:49:38 +0530

    sum of matrix

--- twoDArray.java
+++ twoDArray.java
@@ -4,25 +4,50 @@ import java.util.*;
 public class twoDArray {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter number of rows");
+
+        System.out.println("Enter number of rows");
         int a = sc.nextInt();
-        System.out.println("enter number of columns");
+
+        System.out.println("Enter number of columns");
         int b = sc.nextInt();
-        System.out.println("enter the elements");
-        int matrix[][] = new int[a][b];
+
+        int matrix1[][] = new int[a][b];
+        int matrix2[][] = new int[a][b];
+        int sum[][] = new int[a][b];
+
+        // Matrix 1
+        System.out.println("Enter elements of matrix 1");
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; j++) {
-                matrix[i][j] = sc.nextInt();
-
+                matrix1[i][j] = sc.nextInt();
             }
         }
-        for (int i = 0; i < b; i++) {
-            for (int j = 0; j < a; j++) {
-                System.out.print(matrix[i][j] + " ");
+
+        // Matrix 2
+        System.out.println("Enter elements of matrix 2");
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        // Sum
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        // Print sum
+        System.out.println("Sum of matrices:");
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print(sum[i][j] + " ");
             }
             System.out.println();
         }
-
     }
 }
