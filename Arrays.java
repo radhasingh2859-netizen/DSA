0e5bbeeaa0a12540988f74e5c78bda4a8207b4b3
Radha Singh <radhasingh2859@gmail.com>
2026-08-24 12:53:50 +0530

    sorted or not

--- Arrays.java
+++ Arrays.java
@@ -7,21 +7,24 @@ public class Arrays {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of element");
         int n = sc.nextInt();
-        System.out.println("enter the value of d to move to left ");
-        int d = sc.nextInt();
         int arr[] = new int[n];
-        int arr1[] = new int[n + d];
-
-        System.out.print("enter the element");
+        System.out.println("enter the element ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i = 0; i < arr.length + d; i++) {
-            arr1[i] = arr[(i + d) % n];
+        boolean sorted = true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                sorted = false;
+            }
 
         }
-        for (int i = 0; i < n + d; i++) {
-            System.out.print(arr1[i]);
+        if (sorted) {
+            System.out.println("sorted");
+        } else {
+            System.out.println("not sorted");
         }
+
     }
+
 }
