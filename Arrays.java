c54835b396ca0fd83e409786de0ac0e8278433d6
Radha Singh <radhasingh2859@gmail.com>
2026-08-26 22:02:32 +0530

    bubble sort

--- Arrays.java
+++ Arrays.java
@@ -8,20 +8,25 @@ public class Arrays {
         System.out.println("Enter the size of element");
         int n = sc.nextInt();
         int a[] = new int[n];
-        int b[] = new int[n];
         System.out.println("enter the element");
         for (int i = 0; i < n; i++) {
+
             a[i] = sc.nextInt();
         }
-        for (int i = 0; i < n / 2; i++) {
-            int temp = a[i];
-            a[i] = a[n - i - 1];
-            a[n - i - 1] = temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
 
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+
+                }
+            }
         }
-        System.out.println("reversed array is");
         for (int i = 0; i < n; i++) {
             System.out.print(a[i] + " ");
+
         }
     }
 }
