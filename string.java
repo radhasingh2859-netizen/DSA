18daf2b64bfa583316f1af4ceb7f3104ec6b5a0e
Radha Singh <radhasingh2859@gmail.com>
2026-08-27 21:23:11 +0530

    user name

--- string.java
+++ string.java
@@ -5,26 +5,19 @@ public class string {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the size of the string array ");
-        int a = sc.nextInt();
-        String arr[] = new String[a];
-        for (int i = 0; i < a; i++) {
-            arr[i] = sc.next();
+        System.out.println("enter the email id ");
+        String email = sc.next();
+        String a = " ";
+        for (int i = 0; i < email.length(); i++) {
+            if (email.charAt(i) == '@') {
+                break;
+            } else {
+                a += email.charAt(i);
 
-        }
-        String result = " ";
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < arr[i].length(); j++) {
-
-                if (arr[i].charAt(j) == 'e') {
-                    result += 'i';
-
-                } else {
-                    result += arr[i].charAt(j);
-                }
             }
+
         }
 
-        System.out.println(result);
+        System.out.println(a);
     }
 }
