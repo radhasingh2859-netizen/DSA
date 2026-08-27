9ecf014b1143fbf0c0b1a09514eeb4ee0f403c23
Radha Singh <radhasingh2859@gmail.com>
2026-08-27 09:59:24 +0530

    priint spiral matrix

--- Arrays.java
+++ Arrays.java
@@ -3,30 +3,12 @@ import java.util.*;
 
 public class Arrays {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of element");
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        System.out.println("enter the element");
-        for (int i = 0; i < n; i++) {
-
-            a[i] = sc.nextInt();
-        }
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-
-                if (a[j] > a[j + 1]) {
-                    int temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-
-                }
+    public static void printSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i])
+            {
             }
         }
-        for (int i = 0; i < n; i++) {
-            System.out.print(a[i] + " ");
-
-        }
     }
+
 }
--- twoDArray.java
+++ twoDArray.java
@@ -4,38 +4,54 @@ import java.util.*;
 public class twoDArray {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the rows of the spiral matrix");
+        int n = sc.nextInt();
+        System.out.println("enter the columns of the spiral matrix");
+        int m = sc.nextInt();
+        int a[][] = new int[n][m];
+        System.out.println("enter the elements of the matrix");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = sc.nextInt();
 
-        System.out.println("Enter number of rows");
-        int a = sc.nextInt();
-
-        System.out.println("Enter number of columns");
-        int b = sc.nextInt();
-
-        int matrix1[][] = new int[a][b];
-        int matrix2[][] = new int[a][b];
-        int mul[][] = new int[a][b];
-
-        // Matrix 1
-        System.out.println("Enter elements of matrix 1");
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                matrix1[i][j] = sc.nextInt();
             }
+
         }
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = m - 1;
+        System.out.println("spiral order");
+        while (top <= bottom && left <= right) {
 
-        // Matrix 2
-        System.out.println("Enter elements of matrix 2");
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                matrix2[i][j] = sc.nextInt();
+            // Top row
+            for (int i = left; i <= right; i++) {
+                System.out.print(a[top][i] + " ");
             }
-        }
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                mul[i][j] = (matrix1[i][j] * matrix2[j][i]) + (matrix1[i + 1] * matrix2[j + 1]);
+            top++;
+
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(a[i][right] + " ");
+            }
+            right--;
+
+            // Bottom row
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(a[bottom][i] + " ");
+                }
+                bottom--;
             }
 
+            // Left column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(a[i][left] + " ");
+                }
+                left++;
+            }
         }
     }
+}
