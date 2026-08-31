94c107c9e5ab0e2b1515dfa0672b2162c4ba978c
Radha Singh <radhasingh2859@gmail.com>
2026-08-31 20:53:11 +0530

    find first and last occurance of the character

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,31 @@
 
 public class Recursion {
 
-    public static void printrev(String str, int idx) {
-        if (idx == 0) {
-            System.out.println(str.charAt(idx));
-            return;
+    public static int firstocc = -1;
+    public static int lastocc = -1;
 
+    public static void FirstandLastOcc(int idx, String str, char element) {
+
+        if (idx == str.length()) {
+            System.out.println(firstocc);
+            System.out.println(lastocc);
+            return;
         }
-        System.out.println(str.charAt(idx));
-        printrev(str, idx - 1);
+        char currchar = str.charAt(idx);
+        if (currchar == element) {
+            if (firstocc == -1) {
+                firstocc = idx;
+
+            } else {
+                lastocc = idx;
+            }
+        }
+        FirstandLastOcc(idx + 1, str, element);
+
     }
 
     public static void main(String[] args) {
-        String str = "abcd";
-        printrev(str, str.length() - 1);
+        String str = "abaacaaadaafgdr";
+        FirstandLastOcc(0, str, 'a');
     }
 }
