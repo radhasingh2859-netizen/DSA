b0ea826b31081e488706d456fbc3da07cb87a83d
Radha Singh <radhasingh2859@gmail.com>
2026-08-31 18:00:24 +0530

    print x raise to power n stack height log n

--- Recursion.java
+++ Recursion.java
@@ -1,25 +1,24 @@
 
 public class Recursion {
 
-    public static int printx(int x, int n) {
+    public static int printpower(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
         if (x == 0) {
             return 0;
 
         }
-        if (n == 0) {
-            return 1;
+        if (n % 2 == 0) {
+            return printpower(x, n / 2) * printpower(x, n / 2);
+        } else {
+            return x * printpower(x, n / 2) * printpower(x, n / 2);
 
         }
-        int a = printx(x, n - 1);
-        int b = x * a;
-        return b;
-
     }
 
-    public static void main(String args[]) {
-        int n = 3;
-        int x = 2;
-        System.out.println(printx(x, n));
+    public static void main(String[] args) {
+        int x = 2, n = 3;
+        System.out.println(printpower(x, n));
     }
-
 }
