e1344f35e4feaae073f4453dff034ceff4505dae
Radha Singh <radhasingh2859@gmail.com>
2026-08-31 17:46:12 +0530

    print x raise to power n

--- Recursion.java
+++ Recursion.java
@@ -1,24 +1,25 @@
 
 public class Recursion {
 
-    public static void printfib(int a, int b, int n) {
-
-        if (n == 0) {
-            return;
+    public static int printx(int x, int n) {
+        if (x == 0) {
+            return 0;
 
         }
-        int c = a + b;
-        System.out.println(c);
-        printfib(b, c, n - 1);
-    }
+        if (n == 0) {
+            return 1;
 
-    public static void main(String[] args) {
-        int a = 0;
-        int b = 1;
-        int n = 8;
-        System.out.println(a);
-        System.out.println(b);
-        printfib(a, b, n - 2);
+        }
+        int a = printx(x, n - 1);
+        int b = x * a;
+        return b;
 
     }
+
+    public static void main(String args[]) {
+        int n = 3;
+        int x = 2;
+        System.out.println(printx(x, n));
+    }
+
 }
