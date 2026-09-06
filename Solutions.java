9ff4c13b02347af31364456da81dfc6d086c5fcd
Radha Singh <radhasingh2859@gmail.com>
2026-09-06 13:50:41 +0530

    length of longest substring

--- Solutions.java
+++ Solutions.java
@@ -1,23 +1,32 @@
 
 class Solutions {
 
-    public static int[] twoSum(int num[], int target) {
-        int n = num.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (num[i] + num[j] == target) {
-                    return new int[]{i, j};
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String temp = "";
+            for (int j = i; j < s.length(); j++) {
+                char ch = s.charAt(j);
+                if (!temp.contains(ch + "")) {
+                    temp = temp + ch;
+                } else {
+
+                    break;
                 }
+            }
+            if (temp.length() > max) {
+                max = temp.length();
             }
 
         }
-        return new int[]{};
+        return max;
+
     }
 
     public static void main(String args[]) {
-        int num[] = {1, 2, 3, 4, 5, 6};
-        int[] result = twoSum(num, 9);
-
-        System.out.println(result[0] + " " + result[1]);
+        String s = "pwkew";
+        Solutions obj = new Solutions();
+        System.out.println(obj.lengthOfLongestSubstring(s));
     }
+
 }
