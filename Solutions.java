a2bcc951fc15e3c4fdb24ca0a82d2430831a23b9
Radha Singh <radhasingh2859@gmail.com>
2026-09-07 19:09:18 +0530

    solution ispalindrome

--- Solutions.java
+++ Solutions.java
@@ -1,32 +1,32 @@
 
+import java.util.*;
+
 class Solutions {
 
-    public int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        for (int i = 0; i < s.length(); i++) {
-            String temp = "";
-            for (int j = i; j < s.length(); j++) {
-                char ch = s.charAt(j);
-                if (!temp.contains(ch + "")) {
-                    temp = temp + ch;
-                } else {
-
-                    break;
-                }
-            }
-            if (temp.length() > max) {
-                max = temp.length();
-            }
-
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
         }
-        return max;
+        if (x != 0 && x % 10 == 0) {
+            return false;
+        }
+        int original = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            int lastdigit = x % 10;
+            reverse = reverse * 10 + lastdigit;
+            x = x / 10;
+        }
+        return original == reverse;
 
     }
 
     public static void main(String args[]) {
-        String s = "pwkew";
-        Solutions obj = new Solutions();
-        System.out.println(obj.lengthOfLongestSubstring(s));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of x");
+        int x = sc.nextInt();
+        System.out.println(isPalindrome(x));
     }
 
 }
