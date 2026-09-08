43fb2cd6088e0f40bc1ce78b9400edccf3b91538
Radha Singh <radhasingh2859@gmail.com>
2026-09-08 23:47:33 +0530

    default constructor

---
+++ src/oops/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+    public static void main(String[] args) {
+
+        student a = new student();
+        a.name = "radha";
+
+        System.out.println(a.name);
+    }
+}
---
+++ src/oops/student.java
@@ -0,0 +1,25 @@
+
+public class student {
+
+    public int id;
+    public int age;
+    public String name;
+    public int nos;
+
+    public student() {
+        System.out.println("student default constructor");
+    }
+
+    public void study() {
+        System.out.println(name + " studying");
+    }
+
+    public void sleep() {
+        System.out.println(name + " sleeping");
+    }
+
+    public void bunk() {
+        System.out.println(name + " bunking");
+    }
+
+}
