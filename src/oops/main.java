b735399d569d0ea01f260804370480cbc78aeb2f
Radha Singh <radhasingh2859@gmail.com>
2026-09-10 09:04:37 +0530

    perfect encapsulated

--- src/oops/main.java
+++ src/oops/main.java
@@ -3,20 +3,25 @@ public class main {
 
     public static void main(String[] args) {
 
-        student a = new student(1, 16, "radha", 5);
-        student b = new student(a);
+        student a = new student(1, 10, "radha", 5, 324596334);
+        // student b = new student(a);
 
         // a.name = "radha";
         // a.id = 1;
         // a.age = 14;
         // a.nos = 4;
-        System.out.println(a.name);
-        System.out.println(a.id);
-        a.sleep();
-        b.sleep();
-        System.out.println(b.name);
-        System.out.println(b.id);
-        System.out.println(b.nos);
-        b.bunk();
+        // System.out.println(a.name);
+        // System.out.println(a.id);
+        // a.sleep();
+        // b.sleep();
+        // System.out.println(b.name);
+        // System.out.println(b.id);
+        // System.out.println(b.nos);
+        // b.bunk();
+        // System.out.println(a.mobilenumber);
+        // a.chatting();
+        System.out.println(a.getName());
+        a.setAge(18);
+        System.out.println(a.getAge());
     }
 }
--- src/oops/student.java
+++ src/oops/student.java
@@ -1,39 +1,51 @@
 
 public class student {
 
-    public int id;
-    public int age;
-    public String name;
-    public int nos;
+    private int id;
+    private int age;
+    private String name;
+    private int nos;
+    private long mobilenumber;
 
-    public student(int id, int age, String name, int nos) {
+    public student(int id, int age, String name, int nos, long mobilenumber) {
         System.out.println("student parameterised constructor");
         this.id = id;
         this.age = age;
         this.nos = nos;
         this.name = name;
-
+        this.mobilenumber = mobilenumber;
     }
 
-    public student(student srcobj) {
-        System.out.println("student copy constructor called");
-        this.id = srcobj.id;
-        this.age = srcobj.age;
-        this.name = srcobj.name;
-        this.nos = srcobj.nos;
-
+    public String getName() {
+        return this.name;
     }
 
-    public void study() {
-        System.out.println(name + " studying");
+    public void setAge(int c) {
+        this.age = c;
     }
 
-    public void sleep() {
-        System.out.println(name + " sleeping");
-    }
-
-    public void bunk() {
-        System.out.println(name + " bunking");
+    public int getAge() {
+        return this.age;
     }
 
 }
+
+// public student(student srcobj) {
+//     System.out.println("student copy constructor called");
+//     this.id = srcobj.id;
+//     this.age = srcobj.age;
+//     this.name = srcobj.name;
+//     this.nos = srcobj.nos;
+//  }
+// public void study() {
+//     System.out.println(name + " studying");
+// }
+// public void sleep() {
+//     System.out.println(name + " sleeping");
+// }
+// public void bunk() {
+//     System.out.println(name + " bunking");
+// }
+// private void chatting() {
+//     System.out.println(name + "chatting");
+    // }
